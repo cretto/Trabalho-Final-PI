3f32a152b03a0b812138705b7f104fdd75a6966d
giovaniortolani <giovani_ortolani@hotmail.com>
2016-06-27 00:08:55 -0300

    versao com tudo linkado

--- searcher.py
+++ searcher.py
@@ -1,46 +1,64 @@
-#import the necessary packages
+#-*- coding:utf-8 -*-
+
 import numpy as np 
 import csv
+import math
+import sys
 
 class Searcher:
 	def __init__(self, indexPath):
-		#store our index path
+		# armazena o caminho para o indice 
 		self.indexPath = indexPath
 
-	def search(self, queryFeatures, limit = 10):
-		# initialize our dictionary of results
+	def search(self, queryFeatures, limit, distance):
+		# inicializa o dicionario que contera os resultados
+		# e' do tipo {imageID, score}
+		# imageID (chave) = numero da imagem
+		# score = quao similar a imageID e' em relacao 'a query
 		results = {}
 
-		#open the index file for reading
+		# abre o arquivo com o indice
 		with open(self.indexPath) as f:
-			#initialize the CSV reader
+			# inicializa o leitor de arquivos csv
 			reader = csv.reader(f)
 
-			#loop over the rows in the index
+			# percorre por todas as linhas do arquivo de indice (cada linha e' um vetor de caracteristicas de uma imagem do banco)
 			for row in reader:
-				# parse out the image ID and features, then compute the chi-squared distance between the features in our index
-				# and our query features
+				# recupera o vetor de caracteristicas e armazena em features
 				features = [float(x) for x in row[1:]]
-				d = self.chi2_distance(features, queryFeatures)
+				# calcula a similaridade entre uma imagem e a query
+				if distance.upper() == "CHI2":
+					score = self.chi2_distance(features, queryFeatures)
+				elif distance.upper() == "MANHATTAN": 
+					score = self.manhattan_distance(features, queryFeatures)
+				elif distance.upper() == "EUCLIDEAN":
+					score = self.euclidean_distance(features, queryFeatures)
+				else:
+					sys.exit("Metrica de distancia invalida!")
 
-				# now that we have the distance between the two features vectors, we can update the results dictionary -- the
-				# key is the current image ID in the index and the value is the distance we just computed, representing how
-				# 'similar' the image in the index is to our query
-				results[row[0]] = d
+				# adiciona o resultado ao dicionario
+				results[row[0]] = score
 
-			# close the reader
 			f.close()
 
-		#sort our result, so that the smaller distances (i.e. the more relevant images are at the front of the list)
+		# ordena em ordem decrescente (imagem mais similares primeiro)
 		results = sorted([(v, k) for (k, v) in results.items()])
 
-		# return our (limited) results
 		return results[:limit]
 
-	def chi2_distance(self, histA, histB, eps= 1e-10):
-		# compute the chi-squared distance
-		d = 0.5 * np.sum([((a-b)**2)/(a+b+eps)
-			for (a,b) in zip(histA, histB)])
+	def chi2_distance(self, histA, histB, eps = 1e-10):
+		d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps) for (a, b) in zip(histA, histB)])
 
-		# return the chi-squared distance
 		return d
+
+	def manhattan_distance(self, histA, histB):
+		d = np.sum([abs((a - b)) for (a, b) in zip(histA, histB)])
+
+		return d
+
+	def euclidean_distance(self, histA, histB):
+		d = math.sqrt(np.sum([(a - b) ** 2 for (a, b) in zip(histA, histB)]))
+
+		return d
+
+
